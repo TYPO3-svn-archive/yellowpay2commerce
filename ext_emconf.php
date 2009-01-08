226fb24719ce079bc2aac17292d9a890c268c89e
cedric.spindler <cedric.spindler@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-08 14:11:56 +0000

    Initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yellowpay2commerce/tags/1.0.0@15547 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "yellowpay2commerce"
+#
+# Auto generated 16-12-2008 12:19
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'yellowpay2commerce',
+	'description' => 'Provides the yellowpay payment provider for use with tx_commerce',
+	'category' => 'misc',
+	'author' => 'Cedric Spindler',
+	'author_email' => 'cs@cabag.ch',
+	'author_company' => 'cab services ag',
+	'shy' => '',
+	'dependencies' => 'commerce',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'commerce' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"2f37";s:10:"README.txt";s:4:"99d4";s:31:"class.tx_yellowpay2commerce.php";s:4:"fb9c";s:21:"ext_conf_template.txt";s:4:"0e3a";s:12:"ext_icon.gif";s:4:"0e5d";s:17:"ext_localconf.php";s:4:"0f5d";s:14:"doc/manual.sxw";s:4:"227e";s:19:"doc/wizard_form.dat";s:4:"902c";s:20:"doc/wizard_form.html";s:4:"13e2";}',
+	'suggests' => array(
+	),
+);
+
+?>
