497bb5a46ead505b837d60b77861007d2434c796
cedric.spindler <cedric.spindler@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-01-16 10:49:55 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yellowpay2commerce/trunk@15696 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "yellowpay2commerce"
 #
-# Auto generated 16-12-2008 12:19
+# Auto generated 16-01-2009 11:49
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'constraints' => array(
 		'depends' => array(
 			'commerce' => '',
@@ -39,7 +39,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"2f37";s:10:"README.txt";s:4:"99d4";s:31:"class.tx_yellowpay2commerce.php";s:4:"fb9c";s:21:"ext_conf_template.txt";s:4:"0e3a";s:12:"ext_icon.gif";s:4:"0e5d";s:17:"ext_localconf.php";s:4:"0f5d";s:14:"doc/manual.sxw";s:4:"227e";s:19:"doc/wizard_form.dat";s:4:"902c";s:20:"doc/wizard_form.html";s:4:"13e2";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"2da5";s:10:"README.txt";s:4:"0791";s:31:"class.tx_yellowpay2commerce.php";s:4:"6524";s:21:"ext_conf_template.txt";s:4:"3ba9";s:12:"ext_icon.gif";s:4:"0e5d";s:17:"ext_localconf.php";s:4:"0f5d";s:14:"doc/manual.sxw";s:4:"227e";s:19:"doc/wizard_form.dat";s:4:"902c";s:20:"doc/wizard_form.html";s:4:"13e2";}',
 	'suggests' => array(
 	),
 );
