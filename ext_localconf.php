b5c9daf7b557f65f10899a5b75340e3486b6e60e
dk-at-cabag <dk-at-cabag@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-20 14:59:22 +0000

    complete delete
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/yellowpay2commerce/trunk@25669 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++
@@ -1,9 +0,0 @@
-<?php
-      
-$TYPO3_CONF_VARS['EXTCONF']['commerce']['SYSPRODUCTS']['PAYMENT']['types']['yellowpay'] = array (
-	'path' => t3lib_extmgm::extPath('yellowpay2commerce') .'class.tx_yellowpay2commerce.php',
-	'class' => 'tx_yellowpay2commerce',
-	'type'=> 2,
-);
-
-?>
